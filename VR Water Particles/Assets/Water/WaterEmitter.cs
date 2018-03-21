a89b0a6dbf4b5528f896b5eeceb4561be736680f
Darien Sokolov <darien.sokolov@yahoo.com>
2018-03-21 18:37:03 -0500

    Prefab Emitter Working
    Interactable Particles

---
+++ VR Water Particles/Assets/Water/WaterEmitter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterEmitter : MonoBehaviour {
+  public int countDown = 1000;
+  public GameObject sourceObject;
+  public int currentCount;
+	// Use this for initialization
+	void Start () {
+    currentCount = countDown;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentCount <= 0) {
+      GameObject waterSphere = (GameObject)Instantiate(sourceObject, transform.position, transform.rotation, transform);
+      currentCount = countDown;
+    } else {
+      currentCount--;
+    }
+	}
+}
