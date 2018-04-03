711a5fa62c8398e1fd9ac07f6f80d30e7f5e3cd9
JuJoCoe <jordan.j.coe@gmail.com>
2018-04-03 11:55:02 -0500

    Player Controls and Flying Cam
    Push 5 and 7 to switch between fly cam and player controls

---
+++ VR Water Particles/Assets/VRStandardAssets/Scripts/CamChange.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CamChange : MonoBehaviour {
+	public GameObject[] objects;
+
+
+
+	void Update () {
+		if (Input.GetKeyDown("5")) {
+			objects [0].SetActive (true);
+			objects [1].SetActive (false);
+
+		}
+
+		if (Input.GetKeyDown("7")) {
+			objects [0].SetActive (false);
+			objects [1].SetActive (true);
+		
+		}
+	}
+
+
+
+}
---
+++ VR Water Particles/Assets/VRStandardAssets/Scripts/TeleportTerrain.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportTerrain : MonoBehaviour {
+
+	public void camMainMove(){
+		var player = GameObject.Find("FPSController");
+		player.transform.position = new Vector3(0, 0, 0);
+		Screen.lockCursor = true;
+		var playerevent = GameObject.Find("EventSystem");
+		playerevent.SetActive (false);
+
+	}
+}
+	
