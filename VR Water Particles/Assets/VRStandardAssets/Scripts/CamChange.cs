81969a9ab454e4b1ae1cf81595215277b54580b4
JuJoCoe <jordan.j.coe@gmail.com>
2018-04-05 09:59:36 -0500

    Scene Change Scripts
    Added scene change scripts. Push "9" to go back to starting area.

--- VR Water Particles/Assets/VRStandardAssets/Scripts/CamChange.cs
+++ VR Water Particles/Assets/VRStandardAssets/Scripts/CamChange.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 
 public class CamChange : MonoBehaviour {
@@ -9,19 +10,23 @@ public class CamChange : MonoBehaviour {
 
 
 	void Update () {
-		if (Input.GetKeyDown("5")) {
+		if (Input.GetKeyDown ("5")) {
 			objects [0].SetActive (true);
 			objects [1].SetActive (false);
 
 		}
 
-		if (Input.GetKeyDown("7")) {
+		if (Input.GetKeyDown ("7")) {
 			objects [0].SetActive (false);
 			objects [1].SetActive (true);
 		
 		}
-	}
 
+		if (Input.GetKeyDown ("9")) {
+			Screen.lockCursor = false;
+			SceneManager.LoadScene ("Non-Interactable Particles", LoadSceneMode.Single);
+		}
+	}
 
 
 }
--- VR Water Particles/Assets/VRStandardAssets/Scripts/TeleportTerrain.cs
+++ VR Water Particles/Assets/VRStandardAssets/Scripts/TeleportTerrain.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class TeleportTerrain : MonoBehaviour {
+	public GameObject[] objects;
+
 
 	public void camMainMove(){
-		var player = GameObject.Find("FPSController");
-		player.transform.position = new Vector3(0, 0, 0);
+		var startingcam = GameObject.Find ("StartingCam");
+		startingcam.SetActive (false);
+
+		objects [0].SetActive (true);
+		objects[0].transform.position = new Vector3(50, 200, 50);
 		Screen.lockCursor = true;
+
 		var playerevent = GameObject.Find("TeleportPlayer");
 		playerevent.SetActive (false);
+
+		var canvas = GameObject.Find("Canvas");
+		canvas.SetActive (false);
 
 	}
 }
