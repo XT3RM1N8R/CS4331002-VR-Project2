0cfb8899d6478510163cf1259d68867d820c4daf
JuJoCoe <jordan.j.coe@gmail.com>
2018-04-08 20:44:47 -0500

    Particle Deletion
    Add the script ParticleDeletion.cs to the Fluvio Particle System. You should see option to set distance.

---
+++ VR Water Particles/Assets/Scripts/ParticleDeletion.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System.Linq;
+/// <summary>
+/// Destroys particles that are farther than DistanceToDestroy from the particlesystem emitter
+/// </summary>
+[RequireComponent(typeof(ParticleSystem))]
+public class ParticleDeletion : MonoBehaviour
+{
+	public float DistanceToDestroy;
+	private ParticleSystem cachedSystem;
+	private Vector3 staticPosition;
+	void Start()
+	{
+		cachedSystem = this.GetComponent<ParticleSystem>();
+		staticPosition = this.transform.position;
+	}
+	void Update()
+	{
+		ParticleSystem.Particle[] ps = new ParticleSystem.Particle[cachedSystem.particleCount];
+		cachedSystem.GetParticles(ps);
+		// keep only particles that are within DistanceToDestroy
+		var distanceParticles = ps.Where(p => Vector3.Distance(staticPosition, p.position) < DistanceToDestroy).ToArray();
+		cachedSystem.SetParticles(distanceParticles, distanceParticles.Length);
+	}
+}
